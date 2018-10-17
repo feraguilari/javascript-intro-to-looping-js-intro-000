76ee08dd1491c42c4d76e4d4debb9914124fb21a
Fernando Aguilar Islas <f.aguislas@gmail.com>
2018-10-17 03:06:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,5 @@
-var loops = []
-function forLoop(loops){
+function forLoop(array){
   for (var i = 0; i < 25; i++) {
-  return loops.push('I am ${i} strange loops.')
+  return array.push('I am ${i} strange loops.')
   }
 }
