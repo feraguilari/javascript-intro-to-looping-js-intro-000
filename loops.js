34906faa3f9746d740b0d46a5ff8f6843e9d6e82
Fernando Aguilar Islas <f.aguislas@gmail.com>
2018-10-17 03:23:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,6 +16,12 @@ while (countdown > 0) {
   return `done`
 }
 
-function doWhileLoop(array){
-  
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+   do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
 }
